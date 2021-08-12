c16b62dcd961612172804cc251244918fdcfa8be
Jose Toledo <toledojose60@gmail.com>
2021-08-11 19:14:15 -0700

    design types on sidebar, misc functions needed for later

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 import pyLHD
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+design_type = st.sidebar.selectbox('Choose Design Type:',
+                                   options = ('Random Latin Hypercube Design',
+                                              'Good Lattice Point Design',
+                                              'Orthogonal LHD (Butler 2001)',
+                                              'Orthogonal LHD (Cioppa/Lucas 2007)',
+                                              'Orthogonal LHD (Lin et al. 2009)',
+                                              'Orthogonal LHD (Sun et al. 2010)',
+                                              'Orthogonal LHD (Ye 1998)'))
+
